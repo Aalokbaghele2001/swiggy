76fde6e924f8c7d434c5dac137ad26e27113aa89
Aalokbaghele2001 <aalokebaghele@gmail.com>
2024-06-13 10:58:30 +0530

    Added my project

---
+++ src/components/CategoryMenu.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const CategoryMenu = () => {
+  return (
+    <div className='ml-6'>
+        <h3 className='text-xl font-semibold'>Find the best Food</h3>
+        <div className='my-5 flex gap-3 overflow-x-scroll scroll-smooth lg:overflow-x-hidden'>
+            <button className='px-2 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>ALL</button>
+            <button className='px-2 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>Lunch</button>
+            <button className='px-2 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>Breakfast</button>
+            <button className='px-2 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>Dinner</button>
+            <button className='px-2 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>Snacks</button>
+        </div>
+    </div>
+  )
+}
+
+export default CategoryMenu
