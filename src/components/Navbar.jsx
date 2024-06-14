ac7ca6caa991155c92a46daa634f3c30621b3178
Aalokbaghele2001 <aalokebaghele@gmail.com>
2024-06-14 08:40:44 +0530

    project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,14 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className='flex flex-col lg:flex-row justify-between py-3 mx-6 mb-10'>
-        <div>
-    <h3 className='text-xl font-bold text-gray-600' >{new Date().toUTCString()}</h3>
-    <h1 className='text-2xl font-bold text-gray-700'>Flavoro Foods</h1>
+        <div className='flex'>
+          <div>
+    <h1 className='text-5xl font-bold text-orange-500 '  >Swiggy</h1>
+    <h3 className='text-md font-bold text-orange-400 py-1'>Explore Food Delight</h3>
+    </div>
+
+    <img src="" className='h-14 w-20 my-4  '/>
+   
         </div>
         <div>
     <input  className='p-3 border border-gray-400 text-sm rounded-lg outline-none w-full lg:w-[25vw]'
